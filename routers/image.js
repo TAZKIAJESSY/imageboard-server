ca954cce8363e69ec23de1939d71abc007146683
tazkiajessy <jessy566eee@gmail.com>
2021-03-11 22:06:00 +0100

    paggination is interesting woho

--- routers/image.js
+++ routers/image.js
@@ -4,13 +4,18 @@ const router = new Router(); //crete a router class
 const Image = require("../models").image;
 
 router.get("/", async (req, res, next) => {
+  const limit = Math.min(req.query.limit || 2, 50); // limit indicates how many results are on a page.
+  //Math.min function compares which is min value and returns
+  const offset = req.query.offset || 0;
+
   try {
-    const images = await Image.findAll();
-    res.json(images);
-  } catch (e) {
-    next(e);
+    const result = await Image.findAndCountAll({ limit, offset });
+    res.send({ images: result.rows, total: result.count });
+  } catch (error) {
+    next(error);
   }
 });
+//http :4000/images offset==0 limit==2
 
 router.post("/", async (req, res, next) => {
   try {
@@ -20,6 +25,17 @@ router.post("/", async (req, res, next) => {
     next(error.message);
   }
 });
-module.exports = router;
-
 //http POST :4000/images url=https://picjumbo.com/wp-content/uploads/flying-hearts-on-yellow-background-1080x720.jpg
+
+router.get("/:imageId", async (req, res, next) => {
+  try {
+    const imageId = parseInt(req.params.imageId);
+    const specificImage = await Image.findByPk(imageId);
+    res.send(specificImage);
+  } catch (e) {
+    next(e);
+  }
+});
+//http GET :4000/images/3
+
+module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -5,9 +5,29 @@ const router = new Router();
 router.get("/", async (req, res, next) => {
   try {
     const allUsers = await User.findAll();
-    res.json(allUsers);
+    res.send(allUsers);
   } catch (e) {
     next(e);
   }
 });
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { email, password, fullName } = req.body;
+    if (!fullName || !email || !password) {
+      res.status(400).send("missing parameters");
+    } else {
+      const newUser = await User.create({
+        email,
+        password,
+        fullName,
+      });
+      res.send(newUser);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+//http POST :4000/users fullName=TazkiaJessy email=jessy@hmail.com password=password
+
 module.exports = router;
